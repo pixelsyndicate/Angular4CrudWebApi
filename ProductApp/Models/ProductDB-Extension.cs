afc73977c23f2b1cb8c6957fee25bc96713247ef
Wil Dobson <w.l.w.dobson@gmail.com>
2017-09-26 11:27:27 -0500

    added some form attributes and requirements, added form tag to assist in using angular's built-in clientside validation

--- ProductApp/Models/ProductDB-Extension.cs
+++ ProductApp/Models/ProductDB-Extension.cs
@@ -29,6 +29,49 @@ namespace ProductApp.Models
         {
             List<DbValidationError> list = new List<DbValidationError>();
 
+            // Check ProductName field
+            if (string.IsNullOrEmpty(entity.ProductName))
+            {
+                list.Add(new DbValidationError("ProductName", "Product Name must be filled in."));
+            }
+            else
+            {
+                if (entity.ProductName.ToLower() == entity.ProductName)
+                {
+                    list.Add(new DbValidationError("ProductName", "Product Name must not be all lower case."));
+                }
+                if (entity.ProductName.Length < 4 || entity.ProductName.Length > 150)
+                {
+                    list.Add(new DbValidationError("ProductName", "Product Name must have between 4 and 150 characters."));
+                }
+            }
+
+            // Check IntroductionDate field
+            if (entity.IntroductionDate < DateTime.Now.AddYears(-5))
+            {
+                list.Add(new DbValidationError("IntroductionDate", "Introduction date must be within the last five years."));
+            }
+
+            // Check Price field
+            if (entity.Price <= Convert.ToDecimal(0) ||
+                entity.Price > Convert.ToDecimal(9999.99))
+            {
+                list.Add(new DbValidationError("Price", "Price must be greater than $0 and less than $10, 000."));
+            }
+
+            // Check Url field
+            if (string.IsNullOrEmpty(entity.Url))
+            {
+                list.Add(new DbValidationError("Url", "Url must be filled in."));
+            }
+            else
+            {
+                if (entity.Url.Length < 5 || entity.Url.Length > 255)
+                {
+                    list.Add(new DbValidationError("Url", "Url must be between 5 and 255 characters."));
+                }
+            }
+
             return list;
         }
     }
